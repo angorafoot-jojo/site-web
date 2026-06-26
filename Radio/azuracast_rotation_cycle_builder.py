923332d643b0379226c3d0b82614ec6433073d42
angorafoot-jojo <angorafoot@gmail.com>
2026-06-26 06:55:30 -0400

    Delete azuracast_rotation_cycle_builder.py

--- Radio/azuracast_rotation_cycle_builder.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-raise SystemExit(
-    "\n"
-    "ERREUR : ce script est obsolète.\n"
-    "Utiliser à la place : python Radio/azuracast_rotation_4_blocs.py\n"
-    "Ce script ciblait l'ancienne playlist SERIE_DU_JOUR (architecture abandonnée).\n"
-)
